59ea6ceaeef2163428d8e90fec2f7279999dc9e1
Sa-Daiki <sato-daiki@evolu.tokyo>
2022-11-02 14:18:33 +0900

    🐛  モジュール削除

--- src/components/__tests__/Sample.spec.tsx
+++ src/components/__tests__/Sample.spec.tsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import Sample from "../Sample";
 
-
 describe("サンプルコンポーネント", () => {
   test("should first", () => {
     const { getByText } = render(<Sample />);
