4f2586fbecf4c19b127b9eee0dd7167256146a31
Sa-Daiki <sato-daiki@evolu.tokyo>
2022-11-04 15:27:20 +0900

    ♻️ Api関数の切り出し

--- src/pages/api/population/composition/perYear.ts
+++ src/pages/api/population/composition/perYear.ts
@@ -1,27 +1,7 @@
 /* eslint-disable padding-line-between-statements */
 import { NextApiHandler } from "next";
-import { ApiErrorType, isApiError, switchErrorResponse } from "@/api/error";
-import { API_KEY } from "@/api/key";
-import { PopulationCompositionType } from "@/api/population/usePopulationComposition";
-import { backendApiClient } from "@/lib/ky";
-
-const fetchPopulationCompositionPerYear = async (
-  prefCode: string
-): Promise<PopulationCompositionType | ApiErrorType> => {
-  const response = await backendApiClient
-    .get(
-      "population/composition/perYear?" +
-        new URLSearchParams({ prefCode, cityCode: "-" }).toString(),
-      {
-        headers: { "X-API-KEY": API_KEY },
-      }
-    )
-    .json<PopulationCompositionType | ApiErrorType>();
-  const error = switchErrorResponse(response);
-  if (!error) return response;
-
-  return error;
-};
+import { isApiError } from "@/api/error";
+import { fetchPopulationCompositionPerYear } from "@/api/population/populationCompositionPerYear";
 
 const handler: NextApiHandler = async (req, res) => {
   if (typeof req.query.prefCode !== "string") {
